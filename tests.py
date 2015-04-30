db4055406c1da8104157a19b75ad979df2b4b1aa
= <imghost@ip-10-138-111-216.ec2.internal>
2015-04-30 15:33:16 +0000

    Added python2.7 test

---
+++ tests.py
@@ -0,0 +1,17 @@
+import unittest
+
+import venv_checker
+
+from subprocess import call
+
+class VenvVersionTestCase(unittest.TestCase):
+    def test_python27(self):
+        expected_version = '2.7'
+        path_to_venv = '/tmp/test'
+        call('virtualenv -p /usr/bin/python2.7 ' + path_to_venv)
+        result = venv_checker.get_version(path_to_venv)
+        self.assertEquals(result, expected_version)
+
+if __name__ == '__main__':
+    unittest.main()
+
